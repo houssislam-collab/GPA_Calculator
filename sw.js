57ba25d61e15728254f0a02e27d0541fed72b34f
houssislam-collab <houssislam@gmail.com>
2026-01-09 01:39:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,23 @@
-const cacheName = 'hmd-cache-v1';
+const cacheName = 'hmd-bac-v2';
+// تحديث القائمة لتشمل الاسم الجديد للملف index.html
+const assets = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      return cache.addAll(['./', './index.html']);
+      return cache.addAll(assets);
     })
   );
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
